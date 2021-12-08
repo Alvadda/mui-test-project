82db0d9cc589e950be8cfc28d6b24b14c447cfd5
Alvadda <58438582+Alvadda@users.noreply.github.com>
2021-12-08 22:17:30 +0100

    add darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import CssBaseline from '@mui/material/CssBaseline'
 import { LocalizationProvider } from '@mui/lab'
 import AdapterMoment from '@mui/lab/AdapterMoment'
+import { createTheme, ThemeProvider } from '@mui/material'
 
 import Outline from './Outline'
+import { blueGrey } from '@mui/material/colors'
+import { useState } from 'react'
 
 export const App = () => {
+  const [darkMode, setDarkMode] = useState<boolean>(true)
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      primary: blueGrey,
+    },
+  })
+
+  const switchMode = () => {
+    setDarkMode(!darkMode)
+  }
+
   return (
-    <LocalizationProvider dateAdapter={AdapterMoment}>
-      <CssBaseline />
-      <Outline />
-    </LocalizationProvider>
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <CssBaseline />
+        <Outline darkMode={darkMode} switchTheme={switchMode} />
+      </LocalizationProvider>
+    </ThemeProvider>
   )
 }
--- src/Outline.tsx
+++ src/Outline.tsx
@@ -1,24 +1,53 @@
-import { BottomNavigation, BottomNavigationAction } from '@mui/material'
+import { BottomNavigation, BottomNavigationAction, Switch } from '@mui/material'
+import { makeStyles } from '@mui/styles'
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday'
 import PlayArrowIcon from '@mui/icons-material/PlayArrow'
 import KeyboardIcon from '@mui/icons-material/Keyboard'
+import TimerIcon from '@mui/icons-material/Timer'
 
-import './style/outline.scss'
-import { useState } from 'react'
+import { useState, VFC } from 'react'
 import StartPage from './pages/StartPage'
 import DatePage from './pages/DatePage'
 import InputPage from './pages/InputPage'
 
-const Outline = () => {
+interface OutlineProps {
+  darkMode: boolean
+  switchTheme: () => void
+}
+
+const useStyles: any = makeStyles({
+  outline: {
+    display: 'grid',
+    gridTemplate: `
+    'page' 9fr
+    'footer' 1fr;
+    `,
+    height: '100vh',
+  },
+  page: {
+    gridArea: 'page',
+  },
+  footer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gridArea: 'footer',
+  },
+})
+
+const Outline: VFC<OutlineProps> = ({ darkMode, switchTheme }) => {
   const [page, setPage] = useState<string>('start')
+  const classes = useStyles()
+
   return (
-    <div className="outline">
-      <main className="page">
+    <div className={classes.outline}>
+      <main className={classes.page}>
+        <Switch checked={darkMode} onChange={switchTheme} />
         {page === 'start' && <StartPage />}
         {page === 'date' && <DatePage />}
         {page === 'input' && <InputPage />}
       </main>
-      <footer className="footer">
+      <footer className={classes.footer}>
         <BottomNavigation
           showLabels
           value={page}
@@ -29,6 +58,7 @@ const Outline = () => {
           <BottomNavigationAction label="Start" value="start" icon={<PlayArrowIcon />} />
           <BottomNavigationAction label="Date" value="date" icon={<CalendarTodayIcon />} />
           <BottomNavigationAction label="Input" value="input" icon={<KeyboardIcon />} />
+          <BottomNavigationAction label="Input" value="input" icon={<TimerIcon />} />
         </BottomNavigation>
       </footer>
     </div>
